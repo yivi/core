16853ccfac0dd583bcc51a7353c8b06a0e5d0c1b
Ivan Yivoff <yivi@users.noreply.github.com>
2020-04-17 13:55:11 +0200

    Update ArraySubset::evaluate() signature
    ArraySubset was removed in PHPUnit 9, but the signature of Constraint::evaluate() has been changed to include the return type hint.
    
    It's a tiny change, but without it this is not compatible with v9.

--- src/Bridge/Symfony/Bundle/Test/Constraint/ArraySubset.php
+++ src/Bridge/Symfony/Bundle/Test/Constraint/ArraySubset.php
@@ -43,7 +43,7 @@ final class ArraySubset extends Constraint
     /**
      * {@inheritdoc}
      */
-    public function evaluate($other, string $description = '', bool $returnResult = false)
+    public function evaluate($other, string $description = '', bool $returnResult = false): ?bool
     {
         //type cast $other & $this->subset as an array to allow
         //support in standard array functions.
